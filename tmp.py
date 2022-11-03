a7f0bdc101d09fbd8db33538d636e5e100264c23
Donghoon Kim <dhkim2810@gmail.coml>
2022-11-03 14:14:59 -0700

    Update code for feature extraction experiment

---
+++ tmp.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+from collections import OrderedDict 
+
+class ConvNet(nn.Module):
+    def __init__(self, channel, num_classes, net_width, net_depth, net_act, net_norm, net_pooling, im_size = (32,32)):
+        super(ConvNet, self).__init__()
+
+        self.features, shape_feat = self._make_layers(channel, net_width, net_depth, net_norm, net_act, net_pooling, im_size)
+        num_feat = shape_feat[0]*shape_feat[1]*shape_feat[2]
+        self.classifier = nn.Linear(num_feat, num_classes)
+        self.selected_out = OrderedDict()
+        self.fhooks = []
+    
+        for layer in list(self.features._modules.keys()):
+            if isinstance(getattr(self.features,layer), nn.Conv2d):
+                self.fhooks.append(getattr(self.features,layer).register_forward_hook(self.forward_hook(layer)))
+    
+    def forward_hook(self,layer_name):
+        def hook(module, input, output):
+            self.selected_out[layer_name] = output
+        return hook
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out, self.selected_out
+
+    def embed(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        return out
+
+    def _get_activation(self, net_act):
+        if net_act == 'sigmoid':
+            return nn.Sigmoid()
+        elif net_act == 'relu':
+            return nn.ReLU(inplace=True)
+        elif net_act == 'leakyrelu':
+            return nn.LeakyReLU(negative_slope=0.01)
+        else:
+            exit('unknown activation function: %s'%net_act)
+
+    def _get_pooling(self, net_pooling):
+        if net_pooling == 'maxpooling':
+            return nn.MaxPool2d(kernel_size=2, stride=2)
+        elif net_pooling == 'avgpooling':
+            return nn.AvgPool2d(kernel_size=2, stride=2)
+        elif net_pooling == 'none':
+            return None
+        else:
+            exit('unknown net_pooling: %s'%net_pooling)
+
+    def _get_normlayer(self, net_norm, shape_feat):
+        # shape_feat = (c*h*w)
+        if net_norm == 'batchnorm':
+            return nn.BatchNorm2d(shape_feat[0], affine=True)
+        elif net_norm == 'layernorm':
+            return nn.LayerNorm(shape_feat, elementwise_affine=True)
+        elif net_norm == 'instancenorm':
+            return nn.GroupNorm(shape_feat[0], shape_feat[0], affine=True)
+        elif net_norm == 'groupnorm':
+            return nn.GroupNorm(4, shape_feat[0], affine=True)
+        elif net_norm == 'none':
+            return None
+        else:
+            exit('unknown net_norm: %s'%net_norm)
+
+    def _make_layers(self, channel, net_width, net_depth, net_norm, net_act, net_pooling, im_size):
+        layers = []
+        in_channels = channel
+        if im_size[0] == 28:
+            im_size = (32, 32)
+        shape_feat = [in_channels, im_size[0], im_size[1]]
+        for d in range(net_depth):
+            layers += [nn.Conv2d(in_channels, net_width, kernel_size=3, padding=3 if channel == 1 and d == 0 else 1)]
+            shape_feat[0] = net_width
+            if net_norm != 'none':
+                layers += [self._get_normlayer(net_norm, shape_feat)]
+            layers += [self._get_activation(net_act)]
+            in_channels = net_width
+            if net_pooling != 'none':
+                layers += [self._get_pooling(net_pooling)]
+                shape_feat[1] //= 2
+                shape_feat[2] //= 2
+
+        return nn.Sequential(*layers), shape_feat
+
+class Attention(nn.Module):
+    def __init__(self, args):
+        super(Attention, self).__init__()
+        self.qk_dim = args.qk_dim
+        self.n_t = args.n_t
+        self.linear_trans_s = LinearTransformStudent(args)
+        self.linear_trans_t = LinearTransformTeacher(args)
+
+        self.p_t = nn.Parameter(torch.Tensor(len(args.t_shapes), args.qk_dim))
+        self.p_s = nn.Parameter(torch.Tensor(len(args.s_shapes), args.qk_dim))
+        torch.nn.init.xavier_normal_(self.p_t)
+        torch.nn.init.xavier_normal_(self.p_s)
+
+    def forward(self, g_s, g_t):
+        bilinear_key, h_hat_s_all = self.linear_trans_s(g_s)
+        query, h_t_all = self.linear_trans_t(g_t)
+
+        p_logit = torch.matmul(self.p_t, self.p_s.t())
+
+        logit = torch.add(torch.einsum('bstq,btq->bts', bilinear_key, query), p_logit) / np.sqrt(self.qk_dim)
+        atts = F.softmax(logit, dim=2)  # b x t x s
+        loss = []
+
+        for i, (n, h_t) in enumerate(zip(self.n_t, h_t_all)):
+            h_hat_s = h_hat_s_all[n]
+            diff = self.cal_diff(h_hat_s, h_t, atts[:, i])
+            loss.append(diff)
+        return loss
+
+    def cal_diff(self, v_s, v_t, att):
+        diff = (v_s - v_t.unsqueeze(1)).pow(2).mean(2)
+        diff = torch.mul(diff, att).sum(1).mean()
+        return diff
+
+def main():
+    model = ConvNet(3, 10, 128, 3, 'relu', 'instancenorm', 'avgpooling', im_size=(32,32))
+    
+    x = torch.rand(64,3,32,32)
+    y, features = model(x)
+    print(y.shape)
+    print(len(features), features.keys())
+    print(features['0'].shape)
+
+if __name__=="__main__":
+    main()
